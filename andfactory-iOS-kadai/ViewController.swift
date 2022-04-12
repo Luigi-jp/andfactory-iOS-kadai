419e6b38a00fe7b52cda150a7a62c3f53c8ddc1e
luigi sato <base.basketball0607@gmail.com>
2022-04-12 23:00:21 +0900

    first commit

---
+++ andfactory-iOS-kadai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  andfactory-iOS-kadai
+//
+//  Created by 佐藤瑠偉史 on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ andfactory-iOS-kadaiUITests/andfactory_iOS_kadaiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  andfactory_iOS_kadaiUITestsLaunchTests.swift
+//  andfactory-iOS-kadaiUITests
+//
+//  Created by 佐藤瑠偉史 on 2022/04/12.
+//
+
+import XCTest
+
+class andfactory_iOS_kadaiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
